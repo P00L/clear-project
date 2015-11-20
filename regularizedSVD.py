20073915540746017d9c878b060eebe737af9af1
Paolo <fusari.pool@gmail.com>
2015-11-20 20:16:52 +0100

    pulito codice svd

--- regularizedSVD.py
+++ regularizedSVD.py
@@ -5,23 +5,10 @@
 # U * V^T closely approximates
 # the original matrix (in this case, the utility
 # matrix M)
-#
-#1) costruttore  SvdMatrix class
-#2) readtrainsmaller
-#3) readinratings
-#4) class Rating un sacco di volte
-#5) averagerating
-#6) trainratings
-#   7)for su k
-#   8)train una volta per k
-#       9)predict un po di voolte per k
-#       10)calcrating un po di voolte per k
-#       11)dotproduct un po di voolte per k
-#
-#
 # self.U[uid], self.V[mid]
 #numero user for us = 15374
 #numero item for us = 37142
+#ricorda gli elementi nella matrice U e V sono scalati di -1
 # =======================================
 import math
 import csv
@@ -55,24 +42,24 @@ class SvdMatrix:
     typefile -> 0 if for smaller MovieLens dataset
                 1 if for medium or larger MovieLens dataset
     """
-    def __init__(self, trainfile, nusers, nmovies, r=30, lrate=0.035, regularizer=0.01, typefile=0):
+    def __init__(self, nusers, nmovies, r=50, lrate=0.01, regularizer=0.02):
         self.trainrats = []#la nostra urm sotto forma di lista di oggetti Rating
         self.testrats = []
                 
         self.nusers = nusers
         self.nmovies = nmovies
 
-        if typefile == 0:
-            self.readtrainsmaller(trainfile)
-        elif typefile == 1:
-            self.readtrainlarger(trainfile)
+        self.readinratings(self.trainrats)
 
         # get average rating
-        avg = self.averagerating()
+        self.avg = self.averagerating()
         # set initial values in U, V using square root
         # of average/rank
-        initval = math.sqrt(avg/r)
-        
+        initval = math.sqrt(self.avg/r)
+        #USERBIAS
+        self.Cu= [initval for i in range(nusers)]
+        #ITEM BIAS
+        self.Di=[initval for i in range(nmovies)]
         # U matrix
         self.U = [[initval]*r for i in range(nusers)]
         # V matrix -- easier to store and compute than V^T
@@ -82,7 +69,9 @@ class SvdMatrix:
         self.lrate = lrate
         self.regularizer = regularizer
         self.minimprov = 0.001
-        self.maxepochs = 30            
+        self.maxepochs = 50
+        self.regularizer_2=0.05
+
 
     """
     Returns the dot product of v1 and v2
@@ -95,7 +84,7 @@ class SvdMatrix:
     Ensures returns rating is in range [1,10]
     """
     def calcrating(self, uid, mid):
-        p = self.dotproduct(self.U[uid], self.V[mid])
+        p = self.Cu[uid] + self.Di[mid] + self.dotproduct(self.U[uid], self.V[mid])
         if p > 10:
             p = 10
         elif p < 1:
@@ -141,6 +130,9 @@ class SvdMatrix:
 
             self.U[crating.uid][k] += self.lrate * (err*vTemp - self.regularizer*uTemp)
             self.V[crating.mid][k] += self.lrate * (err*uTemp - self.regularizer*vTemp)
+            self.Cu[crating.uid]  += self.lrate * (err - (self.regularizer_2 * (self.Cu[crating.uid] + self.Di[crating.mid] - self.avg)))
+            self.Di[crating.mid] += self.lrate * (err - (self.regularizer_2 * (self.Cu[crating.uid] + self.Di[crating.mid] - self.avg)))
+
         return math.sqrt(sse/n)
 
     """
@@ -177,55 +169,27 @@ class SvdMatrix:
         return math.sqrt(sse/total)
 
     """
-    Read in the ratings from fname and put in arr
-    Use splitter as delimiter in fname
+    Read in the ratings from train and put in arr
     """
-    def readinratings(self, fname, arr, splitter="\t"):
-        """
-        f = open(fname)
-
-        for line in f:
-            newline = [int(each) for each in line.split(splitter)]
-            userid, movieid, rating = newline[0], newline[1], newline[2]
-            arr.append(Rating(userid, movieid, rating))
-        """
+    def readinratings(self,arr):
         #arr in realta e train rats
         with open('resources/train.csv', 'rt') as f:
             reader = csv.reader(f)
             for row in reader:
                 if row[0]!= 'userId':
-                    userid, movieid, rating = int(row[0]), int(row[1]), int(row[2])
+                    userid, movieid, rating = int(row[0]), int(row[1]), float(row[2])
                     arr.append(Rating(userid, movieid, rating))
-
 
         arr = sorted(arr, key=lambda rating: (rating.uid, rating.mid))
         return len(arr)
-        
-    """
-    Read in the smaller train dataset
-    """
-    def readtrainsmaller(self, fname):
-        return self.readinratings(fname, self.trainrats, splitter="\t")
-        
-    """
-    Read in the large train dataset
-    """
-    def readtrainlarger(self, fname):
-        return self.readinratings(fname, self.trainrats, splitter="::")
-        
-    """
-    Read in the smaller test dataset
-    """
-    def readtestsmaller(self, fname):
-        return self.readinratings(fname, self.testrats, splitter="\t")
-                
-    """
-    Read in the larger test dataset
-    """
-    def readtestlarger(self, fname):
-        return self.readinratings(fname, self.testrats, splitter="::")
 
-
+    """
+    Returns the estimated rating corresponding to userid for movieid
+    DON'T RETURN VALUE BEETWEEEN 1 AND 10 FOR BETTER ORDERING
+    """
+    def predict_rate(self,uid,mid):
+        p = self.Cu[uid] + self.Di[mid] + self.dotproduct(self.U[uid], self.V[mid])
+        return p
 
 with open('resources/test.csv', 'rt') as f:
     reader = csv.reader(f)
@@ -241,16 +205,15 @@ with open('resources/train.csv', 'r') as urm_raw:
             urm.setdefault(int(row[0]),[]).append(int(row[1]))
 
 if __name__ == "__main__":
-    #========= test SvdMatrix class on smallest MovieLENS dataset =========
     init = time.time()
-    #Don't worry about a thing,Cause every little thing gonna be all right........
-    #no dai a parte gli scherzi non guardate che e sbagliato ua.base tanto poi non viene considerato xD
-    svd = SvdMatrix("ua.base", 15374, 37142)#inizializza la classe svdMttrix
+    svd = SvdMatrix(15374, 37142)#inizializza la classe svdMttrix
     svd.trainratings()
     print ("rmsetrain: "+ str(svd.calcrmse(svd.trainrats)))
     print ("time: "+ str(time.time()-init))
+    print("lunghezza V"+str(len(svd.V)))
+    print("lunghezza U"+str(len(svd.U)))
     print("USER 1 ITEM 2738 RATING 1------>stima = "+str(svd.dotproduct(svd.U[2737],svd.V[0])))
-    with open('submission/matrix_factorization_svd.csv', 'w', newline='') as f:
+    with open('submission/matrix_factorization_svd_dd.csv', 'w', newline='') as f:
         my_dict = {}
         rankings = []
         fieldnames = ['userId', 'testItems']
@@ -258,9 +221,10 @@ if __name__ == "__main__":
         w.writeheader()
         for u in range(1,len(user_test_list)):
             rankings = []
-            for i in range(0,37142):
+            for i in range(1,37143):
                 if i not in urm[int(user_test_list[u][0])]:
-                    rankings.append((svd.dotproduct(svd.U[int(user_test_list[u][0])-1],svd.V[i-1]),i))
+                    #-1 perche accesso posizionale nella lista scalato di 1 (vedi classe Ratings)
+                    rankings.append((svd.predict_rate(int(user_test_list[u][0])-1,i-1),i))
             rankings.sort()
             rankings.reverse()
             rankings_cut = rankings[0:5]
