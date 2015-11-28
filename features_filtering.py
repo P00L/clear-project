a9630baa28f32e6bf9dbb0e3b264f41d5cf90355
Paolo <fusari.pool@gmail.com>
2015-11-28 12:41:06 +0100

    features_filtering.py modified.

--- features_filtering.py
+++ features_filtering.py
@@ -3,7 +3,7 @@ import datetime
 from similarity import item_sim
 
 
-def cbf_recommendations(user_ratings, icm_m, sim_skr=20, shrink=10):
+def cbf_recommendations(user_ratings,user, icm_m, sim_skr=20, shrink=10):
     """
     * WARNING:
         # This function is very resources and time consuming if it is done in large batch (e.g in a for loop over
@@ -44,7 +44,8 @@ def cbf_recommendations(user_ratings, icm_m, sim_skr=20, shrink=10):
                     similarity = item_sim(icm_m, movie, other_movie, skr=sim_skr)
                 if similarity != 0:
                     totals.setdefault(other_movie, 0)
-                    totals[other_movie] += user_ratings[movie]*similarity
+                    #totals[other_movie] += user_ratings[movie]*similarity + avg + user_bias[user] + item_bias[movie
+                    totals[other_movie] += user_ratings[movie]*similarity + user_bias[user] + item_bias[movie]
                     sim_sums.setdefault(other_movie, 0)
                     sim_sums[other_movie] += similarity
     # compute the ranking for every movie, but the due to the shrink term the value are not prediction
@@ -71,12 +72,38 @@ with open('resources/icm.csv', 'r') as icm_raw:
         if row[0] != 'itemId':
             icm.setdefault(int(row[0]), {}).setdefault(int(row[1]), 1)
 
+#item bias
+item_bias = {}
+with open('resources/item_bias.csv', 'rt') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row[0] != 'ItemId':
+            item_bias[int(row[0])] = float(row[1])
+
+#user bias
+user_bias = {}
+with open('resources/user_bias.csv', 'rt') as f:
+    reader = csv.reader(f)
+    i = 1
+    for row in reader:
+        if row[0] != 'UserId':
+            user_bias[int(row[0])] = float(row[1])
+
+#riempio user_bias e item_bias per fillare i valori mancanti
+for i in range(1,15374):
+    if i not in user_bias:
+        user_bias[i] = float(0)
+
+for i in range(1,37143):
+    if i not in item_bias:
+        item_bias[i] = float(0)
+
 # urm --> {user:{item:rating}
 with open('resources/train.csv', 'r') as urm_raw:
     reader = csv.reader(urm_raw)
     for row in reader:
         if row[0] != 'userId':
-            urm.setdefault(int(row[0]), {})[int(row[1])] = int(row[2])
+            urm.setdefault(int(row[0]), {})[int(row[1])] = round(float(row[2])+user_bias[int(row[0])]+item_bias[int(row[1])], 5)
 
 
 with open('resources/test.csv', 'rt') as f:
@@ -90,14 +117,14 @@ if possible, to traceback the submission, rename the file in this way
 =======================================================================
 """
 time = datetime.datetime.now()
-with open('submission/cbf_srk25sim_skr15rank.csv', 'w', newline='') as f:
+with open('submission/cbf_srk20sim_skr10rank_biasDoppio.csv', 'w', newline='') as f:
     my_dict = {}
     fieldnames = ['userId', 'testItems']
     w = csv.DictWriter(f, fieldnames=fieldnames)
     w.writeheader()
     for i in range(1, len(user_test_list)):
         my_dict['userId'] = user_test_list[i][0]
-        my_dict['testItems'] = cbf_recommendations(urm[int(user_test_list[i][0])], icm, shrink=15, sim_skr=25)
+        my_dict['testItems'] = cbf_recommendations(urm[int(user_test_list[i][0])],int(user_test_list[i][0]), icm, shrink=10, sim_skr=20)
         w.writerow(my_dict)
         print(str(my_dict['userId']) + "," + str(my_dict['testItems']))
 print(datetime.datetime.now() - time)
