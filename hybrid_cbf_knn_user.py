d2e5cd70ac151a081fc5c1b2de21476cf7007f4b
Alessandro Longoni <alex.longoni1992@gmail.com>
2015-11-30 13:02:16 +0100

    added stuff for knn

--- hybrid_cbf_knn_user.py
+++ hybrid_cbf_knn_user.py
@@ -1,9 +1,9 @@
 import csv
 import datetime
-from similarity import item_sim,cosine_sim
+from similarity import item_sim
 
 
-def cbf_recommendations(user_ratings,user, icm_m, sim_skr=20, shrink=10, w_cbf=0.99, w_urm=0.01):
+def cbf_recommendations(user_ratings, user, icm_m, knn, urm, sim_skr=20, shrink=10, w_cbf=0.9, w_knn=0.1):
     """
     * WARNING:
         # This function is very resources and time consuming if it is done in large batch (e.g in a for loop over
@@ -36,7 +36,10 @@ def cbf_recommendations(user_ratings,user, icm_m, sim_skr=20, shrink=10, w_cbf=0
     totals_cbf = {}  # dizionario {item: sum (rating * similarity)}
     sim_sums_cbf = {}  # dizionario {item: sum (similarity)}
     avg_rec = [(5.0, 33173), (5.0, 33475), (5.0, 1076), (5.0, 35300), (5.0, 15743)]
+    totals_knn = {}
+    sim_sums_knn = {}
 
+    # this is the cbf part of the code
     for other_movie in icm_m:  # scandisco tutti i movie non recensiti dall'user e li confronto con quelli recensiti
         if other_movie not in user_ratings:
             for movie in user_ratings:
@@ -48,45 +51,36 @@ def cbf_recommendations(user_ratings,user, icm_m, sim_skr=20, shrink=10, w_cbf=0
                     totals_cbf[other_movie] += user_ratings[movie]*similarity
                     sim_sums_cbf.setdefault(other_movie, 0.0)
                     sim_sums_cbf[other_movie] += similarity
-    #print('totals_cbf  '+ str(totals_cbf))
-    totals_urm={}
-    simSums_urm={}
-    for other in urm:
-        # don't compare me to myself
-        if other==user: continue
-        similarity_urm = cosine_sim(urm,user,other,6)
-        # ignore scores of zero or lower
-        if similarity_urm<=0: continue
-        for item in urm[other]:
-            # only score movies I haven't seen yet
-            if item not in urm[user] or urm[user][item]==0:
-                # Similarity * Score
-                totals_urm.setdefault(item,0)
-                totals_urm[item]+=urm[other][item]*similarity_urm
-                # Sum of similarities
-                simSums_urm.setdefault(item,0)
-                simSums_urm[item]+=similarity_urm
-    # print('totals_urm  ' + str(totals_urm))
+
+    # this one should be the knn on users
+    if user in knn:
+        for other_user in knn[user]:
+            for movie in urm[other_user]:
+                if movie not in urm[user]:
+                    totals_knn.setdefault(movie, 0.0)
+                    totals_knn[movie] += urm[other_user][movie] * knn[user][other_user]
 
     rankings = {}
     """
     scores_cbf = {}
-    scores_urm = {}
+    scores_knn = {}
 
     for movie in totals_cbf:
         scores_cbf[movie] = totals_cbf[movie]/sim_sums_cbf[movie]
-    for movie in totals_urm:
-        scores_urm[movie] = totals_urm[movie]/simSums_urm[movie]
-    #i totals sono giusti spanna da qui
+    for movie in totals_knn:
+        scores_knn[movie] = totals_knn[movie]/sim_sums_knn[movie]
     """
+
+    # this part is supposed to build up the totoal recommendations
     for movie in totals_cbf:
         rankings[movie] = totals_cbf[movie]*w_cbf
-        if movie in totals_urm:
-            rankings[movie] += totals_urm[movie]*w_urm
+        if movie in totals_knn:
+            rankings[movie] += totals_knn[movie]*w_knn
 
-    for movie in totals_urm:
+    for movie in totals_knn:
         if movie not in rankings:
-            rankings[movie] = totals_urm[movie]*w_urm
+            rankings[movie] = totals_knn[movie]*w_knn
+
     # this one are non normalized rankings
     """
     rankings = [(total, item) for item, total in rankings.items()]"""
@@ -94,7 +88,6 @@ def cbf_recommendations(user_ratings,user, icm_m, sim_skr=20, shrink=10, w_cbf=0
     if you wanna normalized rankings
     rankings = [(round(total/(sim_sums[item]), 3), item) for item, total in rankings.items()]
     """
-
     # compute the ranking for every movie, but the due to the shrink term the value are not prediction
     rankings_final = [(total, item) for item, total in rankings.items()]
     sort_rankings = sorted(rankings_final, key=lambda x: -x[0])[0:5]
@@ -119,7 +112,7 @@ with open('resources/icm.csv', 'r') as icm_raw:
         if row[0] != 'itemId':
             icm.setdefault(int(row[0]), {}).setdefault(int(row[1]), 1)
 
-#item bias
+# item bias
 item_bias = {}
 with open('resources/item_bias.csv', 'rt') as f:
     reader = csv.reader(f)
@@ -145,6 +138,18 @@ for i in range(1,37143):
     if i not in item_bias:
         item_bias[i] = float(0)
 
+"""
+This one is a knn on the users
+"""
+# the dict is {user: {user: sim}}
+knn = {}
+with open('resources/k-nn_cosine_user_15.csv', 'r') as knn_raw:
+    reader = csv.reader(knn_raw)
+    for row in reader:
+        if row[0] != 'userId':
+            knn.setdefault(int(row[0]), {})
+            knn[int(row[0])][int(row[1])] = float(row[2])
+
 # urm --> {user:{item:rating}
 with open('resources/train.csv', 'r') as urm_raw:
     reader = csv.reader(urm_raw)
@@ -164,14 +169,14 @@ if possible, to traceback the submission, rename the file in this way
 =======================================================================
 """
 time = datetime.datetime.now()
-with open('submission/cbf_knn_hybrid_merge_w0.5knn_w0.5cbf.csv', 'w', newline='') as f:
+with open('submission/cbf_knn_hybrid_merge_w0knn_w0cbf.csv', 'w', newline='') as f:
     my_dict = {}
     fieldnames = ['userId', 'testItems']
     w = csv.DictWriter(f, fieldnames=fieldnames)
     w.writeheader()
     for i in range(1, len(user_test_list)):
         my_dict['userId'] = user_test_list[i][0]
-        my_dict['testItems'] = cbf_recommendations(urm[int(user_test_list[i][0])], int(user_test_list[i][0]), icm, shrink=10, sim_skr=20)
+        my_dict['testItems'] = cbf_recommendations(urm[int(user_test_list[i][0])],int(user_test_list[i][0]), icm, knn, urm, shrink=10, sim_skr=20, w_cbf=0.8, w_knn=0.2)
         w.writerow(my_dict)
         print(str(my_dict['userId']) + "," + str(my_dict['testItems']))
 print(datetime.datetime.now() - time)
